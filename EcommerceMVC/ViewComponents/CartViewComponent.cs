d7b64d6c67aecae0ecf6dbf89c8ad66d4f0d73bc
Ho Phuoc Vinh Hung <hophuocvinhhung@gmail.com>
2024-01-27 20:38:02 +0700

    Updated Cart

---
+++ EcommerceMVC/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using EcommerceMVC.Helper;
+using EcommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceMVC.ViewComponents
+{
+    public class CartViewComponent : ViewComponent
+    {
+
+
+        public IViewComponentResult Invoke()
+        {
+
+            var cart = HttpContext.Session.Get<List<CartItem>>
+                      (MyString.CART_KEY) ?? new List<CartItem>();
+
+
+
+            return View("CartPanel", new CartModel
+
+            {
+                Quantity = cart.Sum(p => p.SoLuong),
+                Total = cart.Sum(p => p.ThanhTien)
+            });
+
+        }
+    }
+}
